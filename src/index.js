b399270d8766a826bc97886d0d9750b13a2238f1
RanggaGultom <greview90@gmail.com>
2021-03-28 14:42:18 +0700

    .

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 let fns = {}
+const path = require("path")
 
-const props = require("fs").readdirSync("./src/function/")
+const props = require("fs").readdirSync(path.join(__dirname, "/function/"))
 for(const prop of props) {
     fns[prop] = {}
-    const funs = require("fs").readdirSync(`./src/function/${prop}/`).filter(x => x.endsWith(".js"))
+    const funs = require("fs").readdirSync(path.join(__dirname, `/function/${prop}/`)).filter(x => x.endsWith(".js"))
     for(const fn of funs) {
     fns[prop][fn.split(".")[0]] = require(`./function/${prop}/${fn}`)
     }
